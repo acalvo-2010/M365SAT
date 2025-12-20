6fda36f624c143a7e4f6352e5d4861012bde0f90
Luis Alfonso <158351666+acalvo-2010@users.noreply.github.com>
2025-12-19 21:05:23 -0600

    Interactive
    -interactive

--- core/m365connectors/Connect-MicrosoftSharepoint.ps1
+++ core/m365connectors/Connect-MicrosoftSharepoint.ps1
@@ -38,7 +38,8 @@ function Invoke-MicrosoftSharepointPnPConnection {
             }
             else{
                 Write-Host "Creating EntraIDAppRegistration for PnP.PowerShell..."
-                $ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant "$TenantName.onmicrosoft.com" -Interactive -AzureEnvironment $SpEnvironment -ErrorAction SilentlyContinue
+                # $ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant "$TenantName.onmicrosoft.com" -Interactive -AzureEnvironment $SpEnvironment -ErrorAction SilentlyContinue
+                $ClientId = Register-PnPEntraIDAppForInteractiveLogin -ApplicationName "PnP Rocks" -Tenant "$TenantName.onmicrosoft.com" -UseWebLogin -AzureEnvironment $SpEnvironment -ErrorAction SilentlyContinue
                 $ClientId = $ClientId.'AzureAppId/ClientId'
             }
 
